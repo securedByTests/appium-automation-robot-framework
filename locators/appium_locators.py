ad8e3ca6fbf320daa04c01249ae2f6ce6a67b263
Secured By Tests <119647385+securedByTests@users.noreply.github.com>
2025-02-16 18:42:07 +0530

    Add files via upload

---
+++ locators/appium_locators.py
@@ -0,0 +1,8 @@
+
+## Chrome Launcher ###
+btn_appium_chrome_acceptandcontinue_locator = 'id=com.android.chrome:id/terms_accept'
+btn_appium_chrome_next_locator = 'com.android.chrome:id/next_button'
+lnk_appium_chrome_nothanks_locator = 'com.android.chrome:id/negative_button'
+
+txt_appium_chrome_searchbar_locator = 'com.android.chrome:id/search_box_text'
+lbl_appium_chrome_searchbar_selecturl_locator = '//android.widget.TextView[@resource-id="com.android.chrome:id/line_2"]'
---
+++ locators/common.py
@@ -0,0 +1,11 @@
+appium_browser_url_locator = 'com.android.chrome:id/url_bar'
+
+# User Avatar Locator
+icon_user_avatar_locator = '//span[@class="text-center initials"]'
+lnk_user_logout_locator = '//*[text()="Logout"]'
+btn_submit_link_locator = '//button[contains(@class,"forward-button-container btn btn-default")]'
+
+## Roster Login Creds ##
+txt_roster_username_locator = '//input[@name="admin_user"]'
+txt_roster_password_locator = '//input[@name="admin_pass"]'
+btn_roster_login_locator = '//input[@type="submit"]'
