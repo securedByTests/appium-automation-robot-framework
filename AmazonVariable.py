3faddfbf0e4aceecc8b406313e25e1072d73ecf2
Secured By Tests <119647385+securedByTests@users.noreply.github.com>
2025-02-16 18:33:51 +0530

    Add files via upload

---
+++ AmazonVariable.py
@@ -0,0 +1,6 @@
+APPIUM_SERVER=  'http://127.0.0.1:4723/wd/hub'
+APPIUM_PLATFORM_VERSION=   '8.1'
+APPIUM_PLATFORMNAME=     'Android'
+APPIUM_DEVICE_NAME=   'Nexus 5 API 27'
+APPIUM_PACKAGE_NAME=     'in.amazon.mShop.android.shopping'
+APPIUM_ACTIVITY_NAME=  'com.amazon.micron.StartupActivity'
