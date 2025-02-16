3a6ac4c87d26a8c59d61a11be8b6bcde64cb90b9
Secured By Tests <119647385+securedByTests@users.noreply.github.com>
2025-02-16 18:37:08 +0530

    Create login.py

---
+++ variables/prelogin/login.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# valid credentials
+test_login_password_variable = 'P@ssword1'
+
+# # Invalid credentials
+invalid_email_variable = 'invalid-user-id@test.com'
+invalid_pwd_variable = 'password1'
+not_valid_email_variable = 'webpro'
+
+# Error Messages
+authentication_error_mess_variable = 'Login failed. Please try again.'
+emailreq_error_mess_variable = 'Please enter email address'
+pwdreq_error_mess_variable = 'Please enter password'
+emailnotvalid_mess_variable = 'Email not valid'
