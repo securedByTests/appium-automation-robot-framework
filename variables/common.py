de8634b6992a02e94112059403c81a69883947b0
Secured By Tests <119647385+securedByTests@users.noreply.github.com>
2025-02-16 18:37:54 +0530

    Add files via upload

---
+++ variables/common.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+TEST_TIMEOUT = '180s'
+character_count = '50'
+# Browsers
+BROWSER = 'Chrome'
+HOST_URL = 'http://<selenium-grid-server>/wd/hub'
+environmentToRunTest = 'MOBILE'
+
+# URLs
+login_url_category = '/login'
+signup_url_category = '/'
+
+# Titles
+title_page_variable = 'Page Title'
+
+# Add a website to client variables #
+website_one_variable = 'http://siteone.com'
+site_one_variable = 'siteone.com'
+website_two_variable = 'https://samplesitetwo.com'
+
+default_offset = '200'
